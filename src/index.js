4bbb5d33da4102f39be0da0440b09608b8a7ceef
Melon <guang.xing@liulishuo.com>
2018-12-25 21:33:28 +0800

    feat: complete component

--- src/index.js
+++ src/index.js
@@ -1,3 +1,78 @@
-const a = 1
-console.log(1)
-export default a
+const DEFAULT_OPTIONS = {
+  type: 'easeIn',
+  func: null,
+  start: 0,
+  end: null,
+  duration: 200,
+  onStep: _ => {},
+  onComplete: _ => {},
+};
+const TYPES = {
+  // no easing, no acceleration
+  linear: t => t,
+  // accelerating from zero velocity
+  easeInQuad: t => t * t,
+  // decelerating to zero velocity
+  easeOutQuad: t => t * (2 - t),
+  // acceleration until halfway, then deceleration
+  easeInOutQuad: t => (t < 0.5 ? 2 * t * t : -1 + (4 - 2 * t) * t),
+  // accelerating from zero velocity
+  easeInCubic: t => t * t * t,
+  // decelerating to zero velocity
+  easeOutCubic: t => --t * t * t + 1,
+  // acceleration until halfway, then deceleration
+  easeInOutCubic: t =>
+    t < 0.5 ? 4 * t * t * t : (t - 1) * (2 * t - 2) * (2 * t - 2) + 1,
+  // accelerating from zero velocity
+  easeInQuart: t => t * t * t * t,
+  // decelerating to zero velocity
+  easeOutQuart: t => 1 - --t * t * t * t,
+  // acceleration until halfway, then deceleration
+  easeInOutQuart: t => (t < 0.5 ? 8 * t * t * t * t : 1 - 8 * --t * t * t * t),
+  // accelerating from zero velocity
+  easeInQuint: t => t * t * t * t * t,
+  // decelerating to zero velocity
+  easeOutQuint: t => 1 + --t * t * t * t * t,
+  // acceleration until halfway, then deceleration
+  easeInOutQuint: t =>
+    t < 0.5 ? 16 * t * t * t * t * t : 1 + 16 * --t * t * t * t * t,
+};
+const TIME_INTERVAL = 17;
+
+const loop =
+  window.requestAnimationFrame || (fn => setTimeout(fn, TIME_INTERVAL));
+
+const EasingTrigger = options => {
+  const {
+    type,
+    func,
+    start,
+    end,
+    duration,
+    onStep,
+    onComplete,
+  } = Object.assign({}, DEFAULT_OPTIONS, options);
+  const easingFunc = func || TYPES[type];
+  let time = 0;
+  let isStop = false;
+  let currValue = start;
+  const step = _ => {
+    if (isStop) return;
+    time += TIME_INTERVAL;
+    const x = time / duration;
+    currValue = start + (end - start) * easingFunc(x);
+    if (time <= duration) {
+      onStep(currValue, x);
+      loop(step);
+    } else {
+      onStep(end, x);
+      onComplete();
+    }
+  };
+  loop(step);
+  return _ => {
+    isStop = true;
+  };
+};
+
+export default EasingTrigger;
---
+++ src/test.js
@@ -0,0 +1,64 @@
+import EasingTrigger from './index';
+const myChart = echarts.init(document.getElementById('container'));
+const result = echarts.init(document.getElementById('result'));
+const types = [
+  'linear',
+  'easeInQuad',
+  'easeOutQuad',
+  'easeInOutQuad',
+  'easeInCubic',
+  'easeOutCubic',
+  'easeInOutCubic',
+  'easeInQuart',
+  'easeOutQuart',
+  'easeInOutQuart',
+  'easeInQuint',
+  'easeOutQuint',
+  'easeInOutQuint',
+];
+let number = 0;
+const options = {
+  legend: { data: types, selectedMode: 'single' },
+  xAxis: { type: 'value' },
+  yAxis: { type: 'value' },
+  grid: { top: 100 },
+  series: [],
+};
+const optionsResult = {
+  legend: { data: types },
+  xAxis: { type: 'value' },
+  yAxis: { type: 'value' },
+  grid: { top: 100 },
+  series: [],
+};
+types.forEach(type => {
+  const data = [];
+  EasingTrigger({
+    start: 0,
+    end: 200,
+    type: type,
+    duration: 2000,
+    onStep: (y, x) => {
+      data.push({
+        name: type,
+        value: [+Number(x * 100).toFixed(2), +Number(y * 100).toFixed(2)],
+      });
+    },
+    onComplete() {
+      number++;
+      options.series.push({
+        name: type,
+        type: 'line',
+        data,
+      });
+      optionsResult.series.push({
+        name: type,
+        type: 'line',
+        data,
+      });
+      if (number < types.length) return;
+      myChart.setOption(options);
+      result.setOption(optionsResult);
+    },
+  });
+});
