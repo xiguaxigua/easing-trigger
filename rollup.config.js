7a4da5b283d3b33d528c1cb4ee377084de82d00f
Melon <guang.xing@liulishuo.com>
2018-12-25 19:20:54 +0800

    feat: add build env

---
+++ rollup.config.js
@@ -0,0 +1,25 @@
+import babel from 'rollup-plugin-babel';
+import resolve from 'rollup-plugin-node-resolve';
+
+export default {
+  input: './src/index.js',
+  plugins: [
+    resolve(),
+    babel()
+  ],
+  output: [
+    {
+      format: 'umd',
+      file: 'lib/index.umd.js',
+      name: 'EasingTrigger',
+    },
+    {
+      format: 'cjs',
+      file: 'lib/index.common.js',
+    },
+    {
+      format: 'esm',
+      file: 'lib/index.esm.js',
+    },
+  ],
+};
